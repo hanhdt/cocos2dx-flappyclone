f978ec657decbf93716edd21cd4ee8028ecb9317
hanhdt <tranduchanh.ms@gmail.com>
2015-04-13 16:28:04 +0700

    Add new scenes

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,8 @@
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+#include "SplashScene.h"
+
+// is the game landscape or portrait (true for landscape and false for portrait)
+#define IS_LANDSCAPE false
 
 USING_NS_CC;
 
@@ -7,57 +10,156 @@ AppDelegate::AppDelegate() {
 
 }
 
-AppDelegate::~AppDelegate() 
-{
+AppDelegate::~AppDelegate() {
 }
 
 //if you want a different context,just modify the value of glContextAttrs
 //it will takes effect on all platforms
-void AppDelegate::initGLContextAttrs()
-{
-    //set OpenGL context attributions,now can only set six attributions:
-    //red,green,blue,alpha,depth,stencil
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+void AppDelegate::initGLContextAttrs() {
+	//set OpenGL context attributions,now can only set six attributions:
+	//red,green,blue,alpha,depth,stencil
+	GLContextAttrs glContextAttrs = { 8, 8, 8, 8, 24, 8 };
 
-    GLView::setGLContextAttrs(glContextAttrs);
+	GLView::setGLContextAttrs(glContextAttrs);
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
-    auto director = Director::getInstance();
-    auto glview = director->getOpenGLView();
-    if(!glview) {
-        glview = GLViewImpl::create("My Game");
-        director->setOpenGLView(glview);
-    }
+	// initialize director
+	auto director = Director::getInstance();
+	auto glview = director->getOpenGLView();
+	if (!glview) {
+		glview = GLViewImpl::create("My Game");
+		director->setOpenGLView(glview);
+	}
 
-    // turn on display FPS
-    director->setDisplayStats(true);
+	// turn on display FPS
+	director->setDisplayStats(true);
 
-    // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0 / 60);
+	// set FPS. the default value is 1.0/60 if you don't call this
+	director->setAnimationInterval(1.0 / 60);
 
-    // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+	auto fileUtils = FileUtils::getInstance();
+	auto screenSize = glview->getFrameSize();
+	std::vector < std::string > resDirOrders;
 
-    // run
-    director->runWithScene(scene);
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS) || (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+	// check which assets the devices requires
+	if( 2048 == screenSize.width || 2048 == screenSize.height )
+	{
+		resDirOrders.push_back("ipadhd");
+		resDirOrders.push_back("ipad");
+		resDirOrders.push_back("iphonehd5");
+		resDirOrders.push_back("iphonehd");
+		resDirOrders.push_back("iphone");
 
-    return true;
+		if( true == IS_LANDSCAPE)
+		{
+			glview->setDesignResolutionSize( 2048, 1536, ResolutionPolicy::NO_BORDER);
+		}
+		else
+		{
+			glview->setDesignResolutionSize( 1536, 2048, ResolutionPolicy::NO_BORDER);
+		}
+	}
+	else if( 1024 == screenSize.width || 1024 == screenSize.height)
+	{
+		resDirOrders.push_back("ipad");
+		resDirOrders.push_back("iphonehd5");
+		resDirOrders.push_back("iphonehd");
+		resDirOrders.push_back("iphone");
+
+		if( true == IS_LANDSCAPE)
+		{
+			glview->setDesignResolutionSize( 1024, 768, ResolutionPolicy::NO_BORDER);
+		}
+		else
+		{
+			glview->setDesignResolutionSize( 768, 1024, ResolutionPolicy::NO_BORDER);
+		}
+	}
+	else if ( 1136 == screenSize.width || 1136 == screenSize.height ) // retina iPhone (5 and 5S)
+	{
+		resDirOrders.push_back("iphonehd5");
+		resDirOrders.push_back("iphonehd");
+		resDirOrders.push_back("iphone");
+
+		if( true == IS_LANDSCAPE)
+		{
+			glview->setDesignResolutionSize( 1136, 640, ResolutionPolicy::NO_BORDER);
+		}
+		else
+		{
+			glview->setDesignResolutionSize( 640, 1136, ResolutionPolicy::NO_BORDER);
+		}
+	}
+	else if ( 960 == screenSize.width || 960 == screenSize.height ) // retina iPhone (4 and 4S)
+	{
+		resDirOrders.push_back("iphonehd");
+		resDirOrders.push_back("iphone");
+
+		if( true == IS_LANDSCAPE)
+		{
+			glview->setDesignResolutionSize( 960, 640, ResolutionPolicy::NO_BORDER);
+		}
+		else
+		{
+			glview->setDesignResolutionSize( 640, 960, ResolutionPolicy::NO_BORDER);
+		}
+	}
+	else
+	{
+		resDirOrders.push_back( "iphone" );
+
+		if ( true == IS_LANDSCAPE )
+		{
+			glview->setDesignResolutionSize( 480, 320, ResolutionPolicy::NO_BORDER );
+		}
+		else
+		{
+			glview->setDesignResolutionSize( 320, 480, ResolutionPolicy::NO_BORDER );
+		}
+	}
+#elif (CC_TARGET_PLATFORM == CC_PLATFORM_MAC)
+	resDirOrders.push_back( "ipad" );
+	resDirOrders.push_back( "iphonehd5" );
+	resDirOrders.push_back( "iphonehd" );
+	resDirOrders.push_back( "iphone" );
+
+	if( true == IS_LANDSCAPE)
+	{
+		glview->setFrameSize( 1024, 768 );
+		glview->setDesignResolutionSize( 1024, 768, ResolutionPolicy::NO_BORDER );
+	}
+	else
+	{
+		glview->setFrameSize( 768, 1024);
+		glview->setDesignResolutionSize( 768, 1024, ResolutionPolicy::NO_BORDER);
+	}
+#endif
+
+	fileUtils->setSearchPaths(resDirOrders);
+
+	// create a scene. it's an autorelease object
+	auto scene = SplashScreen::createScene();
+
+	// run
+	director->runWithScene(scene);
+
+	return true;
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground() {
-    Director::getInstance()->stopAnimation();
+	Director::getInstance()->stopAnimation();
 
-    // if you use SimpleAudioEngine, it must be pause
-    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+	// if you use SimpleAudioEngine, it must be pause
+	// SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
-    Director::getInstance()->startAnimation();
+	Director::getInstance()->startAnimation();
 
-    // if you use SimpleAudioEngine, it must resume here
-    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+	// if you use SimpleAudioEngine, it must resume here
+	// SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
